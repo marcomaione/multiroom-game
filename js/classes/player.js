a9bf80abe85c5b996329a946d7f0c68b6c0265e8
marco maione <maionemarco14@gmail.com>
2022-10-18 13:58:51 +0200

    aggiunta personaggio e tastiera

---
+++ js/classes/player.js
@@ -0,0 +1,33 @@
+// creo una classe personaggio
+class Player {
+    constructor() {
+        this.position = {
+            x: 100,
+            y:100,
+        }
+        this.velocity = {
+            x:0,
+            y: 0
+        }
+        this.width = 100
+        this.height = 100
+        this.sides = {
+            bottom: this.position.y + this.height
+        }
+        this.gravity = 1
+    }
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+    update() {
+    // movimento personaggio all interno del canvas
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+        this.sides.bottom = this.position.y + this.height
+        // sopra il fondo del canvas
+        if (this.sides.bottom + this.velocity.y < canvas.height)  {
+            this.velocity.y += this.gravity
+        } else this.velocity.y = 0
+    }
+}
--- main.js
+++ main.js
@@ -5,41 +5,62 @@ const c = canvas.getContext('2d')
 // indico le misure in altezza e larghezza del canvas
 canvas.width = 1024
 canvas.height = 576
-// creo una classe personaggio
-class Player {
-    constructor() {
-        this.position = {
-            x: 100,
-            y:100,
-        }
-        this.width = 100
-        this.height = 100
-        this.sides = {
-            bottom: this.position.y + this.height
-        }
-    }
-    draw() {
-        c.fillStyle = 'red'
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-    update() {
-    // movimento personaggio all interno del canvas
-        if (this.sides.bottom < canvas.height)  {
-            this.position.y++
-            this.sides.bottom = this.position.y + this.height
-        }
-    }
-}
-const player = new Player()
 
-// let bottom = y + 100
+const player = new Player()
+const keys = {
+    ArrowUp : {
+        pressed: false,
+    },
+    ArrowLeft : {
+        pressed: false,
+    },
+    ArrowRigth : {
+        pressed: false,
+    },
+
+}
+
 function animate() {
     window.requestAnimationFrame(animate)
     // do il colore al canvas
     c.fillStyle = 'white'
     c.fillRect(0, 0, canvas.width, canvas.height)
+    player.velocity.x = 0
+    if (keys.ArrowRigth.pressed) {
+        player.velocity.x = 5
+    }else if (keys.ArrowLeft.pressed) {
+        player.velocity.x = -5
+    }
     player.draw()
     player.update()
     
 }
 animate()
+
+// aggiungo la tastiera al gioco
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'ArrowUp':
+            if ( player.velocity.y === 0) player.velocity.y = -20
+        break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+        break
+        case 'ArrowRight':
+            keys.ArrowRigth.pressed = true
+        break
+    }
+
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+        break
+        case 'ArrowRight':
+            keys.ArrowRigth.pressed = false
+        break
+    }
+
+})
